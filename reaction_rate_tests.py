1ec88aae7dc7dcfaa2d2b2132beccba8154ca924
Camilo <camilofosco@gmail.com>
2017-10-15 19:10:47 -0400

    starting tests for reaction rate

---
+++ reaction_rate_tests.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 15 17:50:51 2017
+
+@author: Camilo
+"""
+
+### Tests for ReactionRate:
+    
+def test_reaction_rate():
+    
+    reaction = Reaction(coeffLaws='arr', coeffParams='', reactants, products)
+    reaction_rate = 
