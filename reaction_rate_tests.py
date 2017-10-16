02267db98d2a606e64d71ca4632d255a1815bed7
Camilo <camilofosco@gmail.com>
2017-10-15 21:30:36 -0400

    More tests for ReactionSystem

--- reaction_rate_tests.py
+++ reaction_rate_tests.py
@@ -9,9 +9,9 @@ import Reaction
 import ReactionSystem
 import numpy as np
 
-### Tests for ReactionSystems:
+### Tests for ReactionSystems (rs):
    
-def test_reaction_system_nu_1():
+def test_rs_nu_matrix_creation():
     
     reactions = []
     reactions.append(Reaction(reactants={'A':1,'B':2}, products = {'C':1}))
@@ -25,7 +25,7 @@ def test_reaction_system_nu_1():
     assert( nu_2 == [[0,0],[0,0],[2,0],[0,4]] )
     
 
-def test_reaction_system_progress_rate():
+def test_rs_progress_rate():
     
     reactions = []
     reactions.append(Reaction(reactants={'A':1,'B':2}, products = {'C':2}, coeffLaw = 'const', coeffParams = 10))
@@ -33,15 +33,59 @@ def test_reaction_system_progress_rate():
     
     concentrations = [1,2,1]
     rs = ReactionSystem(reactions, concentrations)
-    prog_rate = rs.get_progress_rate_system()
+    prog_rate = rs.get_progress_rate()
             
     assert(prog_rate==[40,10])
     
-def test_reaction_system_reaction_rate():
+def test_rs_reaction_rate():
     
+    reactions = []
+    reactions.append(Reaction(reactants={'A':1,'B':2}, products = {'C':1}, coeffLaw = 'const', coeffParams = 10))
+    reactions.append(Reaction(reactants={'C':2}, products = {'A':1, 'B':2}, coeffLaw = 'const', coeffParams = 10))
     
-    assert(np.array_equal(rrs.reac_rate_system([1,2,0.5],np.array([[1,2,0]]), np.array([[0,0,1]]), [10]),[-40, -80,  40]))
+    concentrations = [1,2,1]
+    rs = ReactionSystem(reactions, concentrations)
+    prog_rate = rs.get_reac_rate()
+            
+    assert(prog_rate==[-30, -60,  20])
     
+def test_rs_not_enough_concentrations():
     
+    reactions = []
+    reactions.append(Reaction(reactants={'A':1,'B':2}, products = {'C':1}, coeffLaw = 'const', coeffParams = 10))
+    reactions.append(Reaction(reactants={'C':2}, products = {'A':1, 'B':2}, coeffLaw = 'const', coeffParams = 10))
+    
+    concentrations = [1,2]
+    
+    try:
+        rs = ReactionSystem(reactions, concentrations)
+    except Exception as err:
+        assert(type(err)==ValueError)
+            
+def test_rs_too_many_concentrations():
+    
+    reactions = []
+    reactions.append(Reaction(reactants={'A':1,'B':2}, products = {'C':1}, coeffLaw = 'const', coeffParams = 10))
+    reactions.append(Reaction(reactants={'C':2}, products = {'A':1, 'B':2}, coeffLaw = 'const', coeffParams = 10))
+    
+    concentrations = [1,2,3,4]
+    
+    try:
+        rs = ReactionSystem(reactions, concentrations)
+    except Exception as err:
+        assert(type(err)==ValueError)     
+    
+def test_rs_not_enough_params():
+    
+    reactions = []
+    reactions.append(Reaction(reactants={'A':1,'B':2}, products = {'C':1}, coeffLaw = 'arr', coeffParams = [10,10]))
+    reactions.append(Reaction(reactants={'C':2}, products = {'A':1, 'B':2}, coeffLaw = 'const', coeffParams = 10))
+    
+    concentrations = [1,2,3]
+    
+    try:
+        rs = ReactionSystem(reactions, concentrations)
+    except Exception as err:
+        assert(type(err)==ValueError)     
     
     
