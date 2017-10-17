bc1599ead038c14737602d9378e5372aab5c85df
Camilo <camilofosco@gmail.com>
2017-10-17 01:52:54 -0400

    Merging ReactionSystem

--- reaction system/ReactionSystem.py
+++ reaction system/ReactionSystem.py
@@ -3,52 +3,35 @@ import numpy as np
 import Reaction
 
 class ReactionSystem:
-<<<<<<< HEAD
-    """ReactionSystem formats and calculates information for a system of irreversible, elementary reactions, 
-    including progress rate and reaction rate.
-=======
-    '''
-    Class ReactionSystem(reaction_ls, species_ls):
-    This class represents a set of reactions defined by the user. It stores an array
+
+    """
+    
+    Class ReactionSystem(reactions_ls, species_ls):
+    ReactionSystem formats and calculates information for a system of reactions, 
+    including progress rate and reaction rate. It stores an array
     of reactions, and an array of species that interact in these reactions.
-    The role of ReactionSystem is to store reactions. An object of this class
-    can be queried for reaction rates of its species and progress rates of its reactions.
+    An object of this class can be queried for reaction rates of its species and progress rates of its reactions.
     
     INPUTS
     ======
-    reaction_ls: list of Reaction objects.
+    reactions_ls: list of Reaction objects.
+    
     species_ls: optional list of strings, with one specie per string. The list of species
     must match the total set of species in the given Reactions. Order is important
     and is maintained throughout the class.
-        
-    ATTRIBUTES
-    ==========
-    self._num_reactions: number of reactions.
-    self._num_species: number of species
-    self._reaction_ls: list of reactions
-    self._species_ls: list of species
->>>>>>> a80223a7a6b56610c934cf12e77d5fa11b9526fb
     
-    INPUTS:
-    =======
-<<<<<<< HEAD
-    r_ls: list of class Reaction, reactions included in the system
-    
-    e_ls: list of str, concentration element
-    
-    kwargs: non-positional arguments, dictionary of initial state, 
-            including Temperature kwargs['T'] and concentration kwargs['concs']
-    
-    
+    **initial_state: optional parameter indicating the initial state of the reaction.
+    Can contain temperature and an array of concentrations.
+           
     ATTRIBUTES
     ===========
     _num_reaction: integer, number of reactions
     
     _num_element: integer, number of elements
     
-    _r_ls: list of class Reaction, reactions included in the system
+    _reactions_ls: list of class Reaction, reactions included in the system
     
-    _e_ls: list of str, concentration element
+    _species_ls: list of str, concentration element
     
     _T: float, temperature
     
@@ -110,56 +93,29 @@ class ReactionSystem:
     =========
     >>> r_ls = []\
         r_ls.append(Reaction.Reaction(\
-            reactants=dict(H=1,O2=1), products=dict(OH=1,O=1),\
-            coeffLaw='Arrhenius', coeffParams=dict(A=2.0)\
+            reactants=dict('H'=1,'O2'=1), products=dict('OH'=1,'O'=1),\
+            coeffLaw='Arrhenius', coeffParams=dict('A'=2.0)\
         ))\
         r_ls.append(Reaction.Reaction(\
-            reactants=dict(H2=1,O=1), products=dict(OH=1,H=1),\
-            coeffLaw='Arrhenius', coeffParams=dict(A=2.0)\
+            reactants=dict('H2'=1,'O'=1), products=dict('OH'=1,'H'=1),\
+            coeffLaw='Arrhenius', coeffParams=dict('A'=2.0)\
         ))
     >>> d = {}\
         d['T'] = 1\
         d['concs'] = np.array([2, 1, 0.5, 1, 1]).transpose()
-    >>> rs = ReactionSystem.ReactionSystem(e_ls=e_ls, r_ls=r_ls, **d)
+    >>> rs = ReactionSystem.ReactionSystem(reactions,species **d)
     >>> rs.compute_all()
     array([-2.,  2.,  6., -2., -4.])
     """
     
-    def __init__(self, r_ls, e_ls, **initial_state):
-        self._num_reaction = len(r_ls)
-        self._num_element = len(e_ls)
-        self._r_ls = r_ls
-        self._e_ls = e_ls
-        self.set_state(**initial_state)
-=======
-    set_conditions(self, **kwargs):
-        Sets the conditions for the reaction system. Currently: Temperature, Concentrations
-    get_conditions(self):
-        Returns the current conditions.
-    get_reaction_rate_coefs(self):
-        Returns an array of float with each element corresponding to the reaction
-        rate coefficient of each reaction. Queries the Reaction objects. Returns
-        outputs in the same order as the _reaction_ls order.
-    calculate_nu_1(self):
-        Calculates the matrix nu_1, corresponding to the stoichiometric coefs for
-        reactants in all reactions.
-    calculate_nu_2(self):
-        Calculates the matrix nu_2, corresponding to the stoichiometric coefs for
-        products in all reactions.
-    get_progress_rate(self):
-        Calculates and returns the progress rate for each reaction as an array of floats.
-    get_reac_rate(self):
-        Calculates and returns the reaction rate for each species as an array of floats.
-        The order corresponds to the internal order of the _species_ls array.
     
-    '''
-    
-    def __init__(self, reaction_ls, species_ls = []):
-        self._num_reactions = len(reaction_ls)
+    def __init__(self, reactions_ls, species_ls = [], **initial_state):
+        self._num_reactions = len(reactions_ls)
         self._num_species = len(species_ls)
-        self._reaction_ls = reaction_ls
+        self._reactions_ls = _reactions_ls
         self._species_ls = species_ls
->>>>>>> a80223a7a6b56610c934cf12e77d5fa11b9526fb
+        self.set_state(**initial_state)
+
         
     def set_state(self, **kwargs):
         if 'T' in kwargs:
@@ -186,7 +142,7 @@ class ReactionSystem:
     
     def __repr__(self):
         repr_ls = ""
-        for r in self._r_ls:
+        for r in self._reactions_ls:
             repr_ls += repr(r)
 
         return repr_ls
@@ -194,7 +150,7 @@ class ReactionSystem:
     def rateCoeff(self):
         self._k = np.zeros(self._num_reaction)
         
-        for n, r in enumerate(self._r_ls):
+        for n, r in enumerate(self._reactions_ls):
             self._k[n] = r.rateCoeff(T = self._T)
             
         return self._k
@@ -202,8 +158,8 @@ class ReactionSystem:
     def calculate_nu_1(self):
         self._reactants = np.zeros([self._num_element, self._num_reaction])
         
-        for n, r in enumerate(self._r_ls):
-            for idx, e in enumerate(self._e_ls):
+        for n, r in enumerate(self._reactions_ls):
+            for idx, e in enumerate(self._species_ls):
                 self._reactants[idx, n] = r.getReactants()[e] if e in r.getReactants() else 0
                 if self._reactants[idx, n] < 0:
                     raise ValueError("nu_{0}1 = {1}:  Negative stoichiometric coefficients are prohibited!".format(idx, self._reactants[idx, n]))
@@ -213,8 +169,8 @@ class ReactionSystem:
     def calculate_nu_2(self):
         self._products = np.zeros([self._num_element, self._num_reaction])
         
-        for n, r in enumerate(self._r_ls):
-            for idx, e in enumerate(self._e_ls):
+        for n, r in enumerate(self.reactions_ls):
+            for idx, e in enumerate(self._species_ls):
                 self._products[idx, n] = r.getProducts()[e] if e in r.getProducts() else 0
                 if self._products[idx, n] < 0:
                     raise ValueError("nu_{0}2 = {1}:  Negative stoichiometric coefficients are prohibited!".format(idx, self._products[idx, n]))
