8682293af8914a5873ba91e4f39f0f2650daecd6
Camilo <camilofosco@gmail.com>
2017-10-17 00:56:38 -0400

    Documentation for ReactionSystem

--- reaction system/ReactionSystem.py
+++ reaction system/ReactionSystem.py
@@ -2,12 +2,47 @@
 import numpy as np
 
 class ReactionSystem:
+    '''
+    Class ReactionSystem(reaction_ls, species_ls):
+    This class represents a set of reactions defined by the user. It stores an array
+    of reactions, and an array of species that interact in these reactions.
+    The role of ReactionSystem is to store reactions. An object of this class
+    can be queried for reaction rates of its species and progress rates of its reactions.
     
-    def __init__(self, reaction_ls, element_ls):
+    INPUTS
+    ======
+    reaction_ls: list of Reaction objects.
+    species_ls: optional list of strings, with one specie per string. The list of species
+    must match the total set of species in the given Reactions. Order is important
+    and is maintained throughout the class.
+        
+    ATTRIBUTES
+    ==========
+    self._num_reactions = len(reaction_ls)
+    self._num_elements = len(species_ls)
+    self._reaction_ls = reaction_ls
+    self._species_ls = species_ls
+    
+    METHODS
+    =======
+    set_conditions
+    get_conditions
+    get_reaction_rate_coefs
+    calculate_nu_1
+    calculate_nu_2
+    get_progress_rate
+    get_reac_rate
+    
+    
+        
+    '''
+    
+    
+    def __init__(self, reaction_ls, species_ls = []):
         self._num_reactions = len(reaction_ls)
-        self._num_elements = len(element_ls)
+        self._num_elements = len(species_ls)
         self._reaction_ls = reaction_ls
-        self._element_ls = element_ls
+        self._species_ls = species_ls
         
     def set_conditions(self, **kwargs):
         # C: I believe that this should be changed to clearly indicate what are the possible inputs.
@@ -18,8 +53,8 @@ class ReactionSystem:
             self._T = kwargs['T']
             
         if 'concs' in kwargs:
-            if (kwargs['concs'] != len(self._reaction_ls)):
-                raise ValueError('The number of concentrations is different than the number of species')
+            if (len(kwargs['concs']) != len(self.species_ls)):
+                raise ValueError('The number of concentrations ({}) is different than the number of species ({})'.format(len(kwargs['concs']), len(self._species_ls))
             for conc in kwargs['concs']:
                 if conc < 0:
                     raise ValueError('Negative concentrations are prohibited.')
@@ -27,7 +62,7 @@ class ReactionSystem:
             self._concentration_ls = kwargs['concs']
     
     def get_conditions(self):
-        # I believe the dictionary is not necessary in this case
+        # I think that a dictionary is not necessary in this case.
         return dict(T=self._T, concs=self._concentration_ls)
     
     def get_reaction_rate_coefs(self):
@@ -40,7 +75,7 @@ class ReactionSystem:
     def calculate_nu_1(self):
         self._reactants = np.zeros([self._num_element, self._num_reaction])
         for n, r in enumerate(self._reaction_ls):
-            for idx, e in enumerate(self._element_ls):
+            for idx, e in enumerate(self._species_ls):
                 self._reactants[idx, n] = r.getReactants()[e] if e in r.getReactants() else 0
         
         return self._reactants
@@ -48,7 +83,7 @@ class ReactionSystem:
     def calculate_nu_2(self):
         self._products = np.zeros([self._num_element, self._num_reaction])
         for n, r in enumerate(self._reaction_ls):
-            for idx, e in enumerate(self._element_ls):
+            for idx, e in enumerate(self._species_ls):
                 self._products[idx, n] = r.getProducats()[e] if e in r.getProducats() else 0
                 
         return self._products
