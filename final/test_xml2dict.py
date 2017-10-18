33634633808977f8ab1f947d61967879b4e0a3d1
Baptiste Lemaire <baptistelemaire@Baptistes-MacBook-Pro.local>
2017-10-17 21:47:11 -0400

    Troubleshooting in Travis for xml2dict test file

--- final/test_xml2dict.py
+++ final/test_xml2dict.py
@@ -12,25 +12,25 @@ def test_class1():
     
 def test_class2():
     r = xml2dict()
-    r.parse('rxns.xml')
+    r.parse('./final/rxns.xml')
     assert(r.get_info()[0][1] == 'O')    
     
 def test_class3():
     r = xml2dict()
-    r.parse('rxns.xml')
+    r.parse('./final/rxns.xml')
     assert(r.get_info()[1][1]['products'] == {'H': 1, 'OH': 1})
     
 def test_class5():
     r = xml2dict()
-    r.parse('rxns2.xml')
+    r.parse('./final/rxns2.xml')
     assert(r.get_info()[1][1]['reactants'] == {'HO2': 1, 'OH': 1}) 
     
 def test_class6():
     r = xml2dict()
-    r.parse('rxns2.xml')
+    r.parse('./final/rxns2.xml')
     assert(r.get_info()[1][2]['ID'] == 'reaction03') 
     
 def test_class7():
     r = xml2dict()
-    r.parse('rxns2.xml')
+    r.parse('./final/rxns2.xml')
     assert(r.get_info()[1][2]['reversible'] == 'no') 
