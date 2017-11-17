bdd6b179cb69a7844f0a8860766b30f4306e1369
Yiqi Xie <shell@Yiqis-MacBook-Pro.local>
2017-11-17 15:19:15 -0500

    minor modification to backward laws

--- reversible/coeff_laws.py
+++ reversible/coeff_laws.py
@@ -469,58 +469,53 @@ class BackwardLaw:
     to be of some use.
     """
 
-    def __init__(self, a, nu):
-        self.a = a
-        self.nu = nu
+    def __init__(self):
         self.p0 = 1.0e+05
         self.R = 8.3144598
 
-    def Cp_over_R(self, T):
+    def Cp_over_R(self, a, T):
 
         # WARNING:  This line will depend on your own data structures!
         # Be careful to get the correct coefficients for the appropriate 
         # temperature range.  That is, for T <= Tmid get the low temperature 
         # range coeffs and for T > Tmid get the high temperature range coeffs.
-        a = self.a
 
-        Cp_R = (a[:,0] + a[:,1] * T + a[:,2] * T**2.0 
+        Cp_R = (a[:,0] + a[:,1] * T + a[:,2] * T**2.0 \
                 + a[:,3] * T**3.0 + a[:,4] * T**4.0)
 
         return Cp_R
 
-    def H_over_RT(self, T):
+    def H_over_RT(self, a, T):
 
         # WARNING:  This line will depend on your own data structures!
         # Be careful to get the correct coefficients for the appropriate 
         # temperature range.  That is, for T <= Tmid get the low temperature 
         # range coeffs and for T > Tmid get the high temperature range coeffs.
-        a = self.a
 
-        H_RT = (a[:,0] + a[:,1] * T / 2.0 + a[:,2] * T**2.0 / 3.0 
-                + a[:,3] * T**3.0 / 4.0 + a[:,4] * T**4.0 / 5.0 
+        H_RT = (a[:,0] + a[:,1] * T / 2.0 + a[:,2] * T**2.0 / 3.0 \
+                + a[:,3] * T**3.0 / 4.0 + a[:,4] * T**4.0 / 5.0 \
                 + a[:,5] / T)
 
         return H_RT
                
 
-    def S_over_R(self, T):
+    def S_over_R(self, a, T):
 
         # WARNING:  This line will depend on your own data structures!
         # Be careful to get the correct coefficients for the appropriate 
         # temperature range.  That is, for T <= Tmid get the low temperature 
         # range coeffs and for T > Tmid get the high temperature range coeffs.
-        a = self.a
 
-        S_R = (a[:,0] * np.log(T) + a[:,1] * T + a[:,2] * T**2.0 / 2.0 
+        S_R = (a[:,0] * np.log(T) + a[:,1] * T + a[:,2] * T**2.0 / 2.0 \
                + a[:,3] * T**3.0 / 3.0 + a[:,4] * T**4.0 / 4.0 + a[:,6])
 
         return S_R
 
-    def equilibrium_coeffs(self, T):
+    def equilibrium_coeffs(self, a, nu, T):
 
         # Change in enthalpy and entropy for each reaction
-        delta_H_over_RT = np.dot(self.nu.T, self.H_over_RT(T))
-        delta_S_over_R = np.dot(self.nu.T, self.S_over_R(T))
+        delta_H_over_RT = np.dot(nu.T, self.H_over_RT(a, T))
+        delta_S_over_R = np.dot(nu.T, self.S_over_R(a, T))
 
         # Negative of change in Gibbs free energy for each reaction 
         delta_G_over_RT = delta_S_over_R - delta_H_over_RT
