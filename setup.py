f7d9e650ae1087e236bb208070139d2cd496c4b3
Baptiste Lemaire <baptistelemaire@Baptistes-MacBook-Pro.local>
2017-11-19 22:18:22 -0500

    Minor changes in setup.py and setup.cfg

--- setup.py
+++ setup.py
@@ -2,23 +2,23 @@ from setuptools import setup
 import sys
 from setuptools.command.test import test as TestCommand
 
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+#class PyTest(TestCommand):
+#    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+#
+#    def initialize_options(self):
+#        TestCommand.initialize_options(self)
+#        self.pytest_args = []
 
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
+#    def finalize_options(self):
+#        TestCommand.finalize_options(self)
+#        self.test_args = []
+#        self.test_suite = True
+#
+#    def run_tests(self):
+#        #import here, cause outside the eggs aren't loaded
+#        import pytest
+#        errno = pytest.main(self.pytest_args)
+#        sys.exit(errno)
 
 setup(name='chemkin_CS207_G9',
       version='1.63',
@@ -26,8 +26,10 @@ setup(name='chemkin_CS207_G9',
       url='https://github.com/cs207group9/cs207-FinalProject/tree/master/chemkin_CS207_G9',
       author='Group 9 - CF, YX, JL, BJL',
       author_email='blemaire@g.harvard.edu',
-      tests_require=['pytest'],
-      cmdclass = {'test': PyTest},      
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],      
+      #tests_require=['pytest'],
+      #cmdclass = {'test': PyTest},       
       license='Harvard University',
       packages=['chemkin_CS207_G9'],
       zip_safe=False)
